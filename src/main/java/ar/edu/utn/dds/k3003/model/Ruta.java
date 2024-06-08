fb70d549fb421af62cf63b4f2f6fe43eb7af9242
Guadalupe <gabdelnour@frba.utn.edu.ar>
2024-06-08 19:06:29 -0300

    primer avance persistencia

--- src/main/java/ar/edu/utn/dds/k3003/model/Ruta.java
+++ src/main/java/ar/edu/utn/dds/k3003/model/Ruta.java
@@ -3,15 +3,27 @@ package ar.edu.utn.dds.k3003.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 @Getter
 @Setter
+@Table(name = "rutas")
 public class Ruta {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "colaborador_Id")
     private Long colaboradorId;
+    @Column(name = "heladera_Id_Origen")
     private Integer heladeraIdOrigen;
+    @Column(name = "heladera_Id_Destino")
     private Integer heladeraIdDestino;
 
+    @Transient
+    private List<Traslado> traslados;
 
     public Ruta(Long colaboradorId, Integer heladeraIdOrigen, Integer heladeraIdDestino){
         //el constructor
@@ -19,6 +31,10 @@ public class Ruta {
         this.colaboradorId = colaboradorId;
         this.heladeraIdOrigen = heladeraIdOrigen;
         this.heladeraIdDestino = heladeraIdDestino;
+    }
+
+    public Ruta(){
+
     }
 
 
